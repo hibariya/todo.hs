62fa47a537720e0d707b56b3bda0516680c4ff00
hibariya <celluloid.key@gmail.com>
2013-08-13 15:25:05 +0900

    Is it OK?
    ほぼ写経

---
+++ src/Main.hs
@@ -0,0 +1,43 @@
+import System.Environment
+import System.IO
+import System.Directory
+import Data.List
+import Control.Exception
+
+main = do
+  (command:args) <- getArgs
+  dispatch command args
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add"    = addTask
+dispatch "list"   = listTasks
+dispatch "remove" = removeTask
+
+addTask :: [String] -> IO ()
+addTask [path, task] = appendFile path (task ++ "\n")
+
+listTasks :: [String] -> IO ()
+listTasks [path] = do
+  contents <- readFile path
+  let tasks          = lines contents
+      tasksWithIndex = zipWith (\i line -> show i ++ ":\t" ++ line) [0..] tasks
+
+  putStr $ unlines tasksWithIndex
+
+removeTask :: [String] -> IO ()
+removeTask [path, indexStr] = do
+  contents <- readFile path
+  let tasks       = lines contents
+      index       = read indexStr
+      newContents = unlines $ delete (tasks !! index) tasks
+
+  bracketOnError (openTempFile "." "temp")
+    (\(tempPath, tempHandle) -> do
+      hClose tempHandle
+      removeFile tempPath)
+
+    (\(tempPath, tempHandle) -> do
+      hPutStr tempHandle newContents
+      hClose tempHandle
+      removeFile path
+      renameFile tempPath path)
